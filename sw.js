3ae2847aae2a9bdaa0b00e7a3d62bef08b752f55
rinsefreerice <rinsefreerice@gmail.com>
2026-06-16 06:15:42 +0900

    initial commit

---
+++ sw.js
@@ -0,0 +1,41 @@
+// Bump CACHE version when app files change to force cache refresh
+const CACHE = 'wm-v4';
+
+const LOCAL_ASSETS = [
+  './',
+  './manifest.json',
+  './icon.svg',
+];
+
+const CDN_ASSETS = [
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/htm@3/dist/htm.js',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await cache.addAll(LOCAL_ASSETS);
+    await Promise.allSettled(CDN_ASSETS.map(url => cache.add(url)));
+    await self.skipWaiting();
+  })());
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  // Never intercept WebDAV sync requests
+  if (e.request.url.includes('/remote.php/')) return;
+
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
